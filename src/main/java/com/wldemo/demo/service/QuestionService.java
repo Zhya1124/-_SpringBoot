7497b80092ee25512ab9bb2cdf0f272f9ba67c7e
Zhya <Zhya1124@live.com>
2020-03-17 19:15:37 +0800

    MyBatisGenerator集成完毕

--- src/main/java/com/wldemo/demo/service/QuestionService.java
+++ src/main/java/com/wldemo/demo/service/QuestionService.java
@@ -5,7 +5,9 @@ import com.wldemo.demo.dto.QuestionDTO;
 import com.wldemo.demo.mapper.QuestionMapper;
 import com.wldemo.demo.mapper.UserMapper;
 import com.wldemo.demo.model.Question;
+import com.wldemo.demo.model.QuestionExample;
 import com.wldemo.demo.model.User;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,7 +26,7 @@ public class QuestionService {  //组装user和question
 
         PaginationDTO paginationDTO = new PaginationDTO();//分页DTO
         Integer totalPage;
-        Integer totalCount = questionMapper.count();//查询获取问题总数
+        Integer totalCount = (int)questionMapper.countByExample(new QuestionExample());//查询获取问题总数
         //该用户总页数(之前在paginationService里现在单独拿出来)
         if (totalCount % size == 0) {
             totalPage = totalCount / size;
@@ -40,9 +42,10 @@ public class QuestionService {  //组装user和question
         }
         paginationDTO.setPagination(totalPage,page);//计算好对应的标志和页数
         Integer offset = size * (page - 1);//计算offset
-        List<Question> questionList = questionMapper.list(offset, size);//查询出的是一页需要的量
+        //List<Question> questionList = questionMapper.list(offset, size);//查询出的是一页需要的量
+        List<Question> questions = questionMapper.selectByExampleWithRowbounds(new QuestionExample(), new RowBounds(offset, size));
         List<QuestionDTO> questionDTOList = new ArrayList<>();
-        for (Question question : questionList) {//查每个问题的user并加入到dto中
+        for (Question question : questions) {//查每个问题的user并加入到dto中
             User user = userMapper.selectByPrimaryKey(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
             BeanUtils.copyProperties(question, questionDTO);//拷贝一个对象的全部属性到另一个对象，避免写太多set
@@ -57,8 +60,10 @@ public class QuestionService {  //组装user和question
     public PaginationDTO list(Integer userId, Integer page, Integer size) {//查出某个id对应作者的问题
         PaginationDTO paginationDTO = new PaginationDTO();//分页DTO
         Integer totalPage;
-        Integer totalCount = questionMapper.countByUserId(userId);//查询某用户的问题总数
-
+        //Integer totalCount = questionMapper.countByUserId(userId);//查询某用户的问题总数
+        QuestionExample example = new QuestionExample();
+        example.createCriteria().andCreatorEqualTo(userId);
+        Integer totalCount = (int)questionMapper.countByExample(example);
         //该用户总页数(之前在paginationService里现在单独拿出来)
         if (totalCount % size == 0) {
             totalPage = totalCount / size;
@@ -75,9 +80,12 @@ public class QuestionService {  //组装user和question
         paginationDTO.setPagination(totalPage,page);//计算好对应的标志和页数
         //5*(i-1)
         Integer offset = size * (page - 1);//计算offset
-        List<Question> questionList = questionMapper.listByUserId(userId,offset, size);//查询出的是一页需要的量
+        //List<Question> questionList = questionMapper.listByUserId(userId,offset, size);//查询出的是一页需要的量
+        QuestionExample questionExample = new QuestionExample();
+        questionExample.createCriteria().andCreatorEqualTo(userId);
+        List<Question> questions = questionMapper.selectByExampleWithRowbounds(questionExample, new RowBounds(offset, size));
         List<QuestionDTO> questionDTOList = new ArrayList<>();
-        for (Question question : questionList) {//查每个问题的user并加入到dto中
+        for (Question question : questions) {//查每个问题的user并加入到dto中
             User user = userMapper.selectByPrimaryKey(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
             BeanUtils.copyProperties(question, questionDTO);//拷贝一个对象的全部属性到另一个对象，避免写太多set
@@ -90,7 +98,7 @@ public class QuestionService {  //组装user和question
     }
 
     public QuestionDTO getById(Integer id) {
-        Question question = questionMapper.getById(id);
+        Question question = questionMapper.selectByPrimaryKey(id);
         QuestionDTO questionDTO = new QuestionDTO();
         BeanUtils.copyProperties(question,questionDTO);
         User user = userMapper.selectByPrimaryKey(question.getCreator());
@@ -103,11 +111,18 @@ public class QuestionService {  //组装user和question
             //创建
             question.setGmtCreate(System.currentTimeMillis());
             question.setGmtModified(question.getGmtCreate());
-            questionMapper.create(question);
+            questionMapper.insertSelective(question);//insert插入有点问题，浏览回复点赞三个数量为null，改成这样就好了
         }else{
             //更新
-            question.setGmtModified(System.currentTimeMillis());
-            questionMapper.update(question);
+            //question.setGmtModified(System.currentTimeMillis());
+            Question updateQuestion = new Question();
+            updateQuestion.setGmtModified(System.currentTimeMillis());
+            updateQuestion.setTitle(question.getTitle());
+            updateQuestion.setDescription(question.getDescription());
+            updateQuestion.setTag(question.getTag());
+            QuestionExample example = new QuestionExample();
+            example.createCriteria().andIdEqualTo(question.getId());//更新原问题同id的问题
+            questionMapper.updateByExampleSelective(updateQuestion, example);
         }
     }
 }
