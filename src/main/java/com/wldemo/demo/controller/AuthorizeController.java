2006216558f8bc1a46ea1cce6861e1f58164f013
Zhya <Zhya1124@live.com>
2020-04-12 11:25:22 +0800

    追加日志功能

--- src/main/java/com/wldemo/demo/controller/AuthorizeController.java
+++ src/main/java/com/wldemo/demo/controller/AuthorizeController.java
@@ -6,6 +6,7 @@ import com.wldemo.demo.mapper.UserMapper;
 import com.wldemo.demo.model.User;
 import com.wldemo.demo.provider.GithubProvider;
 import com.wldemo.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -18,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
+@Slf4j
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
@@ -57,6 +59,7 @@ public class AuthorizeController {
             return "redirect:/";
         }
         else{
+            log.error("callback get github error,{}",githubUser);
             //登录失败，重新登陆
             return "redirect:/";
         }
