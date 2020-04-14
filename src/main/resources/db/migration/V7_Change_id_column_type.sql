fa77a0ec2010b95523b1e2f3a9c6cdad36ba67cf
Zhya <Zhya1124@live.com>
2020-04-14 12:58:37 +0800

    修复数据库脚本V7

--- src/main/resources/db/migration/V7_Change_id_column_type.sql
+++
@@ -1,4 +0,0 @@
-alter table question modify id bigint auto_increment not null;
-alter table `user` modify id bigint auto_increment not null;
-alter table question modify creator bigint auto_increment not null;
-alter table comment modify commentator bigint auto_increment not null;
